1f3a629d3cb7e83568f896cf89147c53a464bbda
Andy Ke <andyke00@gmail.com>
2021-10-13 13:24:14 -0400

    page accueil en WP

---
+++ accueil.php
@@ -0,0 +1,176 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WebLogic
+ */
+
+get_header();
+
+?>
+
+<h1>Accueil</h1>
+
+<div class="texteIntro">
+    <p>
+    La technique cherche à se spécialiser dan deux domaines : la programmation intéractive et la création médiatique.
+     Pi ouain tsé le TIM est full cool, bla bla bla bla lb blablabl basb sablalsbabslsbab bl sbb la beaucoup de texte a remplir dans cette section probablement haha, j'ai pas hate.
+    </p>
+</div>
+
+<h3>Conditions d'admission</h3>
+
+<ul>
+    <li>Tu dois avoir ton diplôme d’études secondaires (DES) ou une formation jugée équivalente.</li>
+    <li>Tu dois avoir les cours préalables requis au programme que tu as choisi ou être en voie de les réussir.</li>
+    <li>Tu dois réussir, pour certains programmes, les tests ou satisfaire aux conditions particulières d’admission du collège.</li>
+</ul>
+
+<h3>Courts offerts</h3>
+
+<div class="ligneTemps">
+
+  <div class="sessionUn">Session 1
+     <div class="carte">
+            <h3 class="carte__titre">Cours 1</h3>
+             <p class="carte__description"> Le cours 1 consiste à ...</p>
+    </div>
+
+     <div class="carte">
+            <h3 class="carte__titre">Cours 2</h3>
+             <p class="carte__description"> Le cours 2 consiste à ...</p>
+    </div>
+
+     <div class="carte">
+            <h3 class="carte__titre">Cours 3</h3>
+             <p class="carte__description"> Le cours 3 consiste à ...</p>
+    </div>
+
+     <div class="carte">
+            <h3 class="carte__titre">Cours 4</h3>
+             <p class="carte__description"> Le cours 4 consiste à ...</p>
+    </div>
+  </div>
+ 
+
+  <div class="sessionDeux">Session 2
+     <div class="carte">
+            <h3 class="carte__titre">Cours 1</h3>
+             <p class="carte__description"> Le cours 1 consiste à ...</p>
+    </div>
+
+     <div class="carte">
+            <h3 class="carte__titre">Cours 2</h3>
+             <p class="carte__description"> Le cours 2 consiste à ...</p>
+    </div>
+
+     <div class="carte">
+            <h3 class="carte__titre">Cours 3</h3>
+             <p class="carte__description"> Le cours 3 consiste à ...</p>
+    </div>
+
+     <div class="carte">
+            <h3 class="carte__titre">Cours 4</h3>
+             <p class="carte__description"> Le cours 4 consiste à ...</p>
+    </div>
+</div>
+
+  <div class="sessionTrois">Session 3
+     <div class="carte">
+            <h3 class="carte__titre">Cours 1</h3>
+             <p class="carte__description"> Le cours 1 consiste à ...</p>
+    </div>
+
+     <div class="carte">
+            <h3 class="carte__titre">Cours 2</h3>
+             <p class="carte__description"> Le cours 2 consiste à ...</p>
+    </div>
+
+     <div class="carte">
+            <h3 class="carte__titre">Cours 3</h3>
+             <p class="carte__description"> Le cours 3 consiste à ...</p>
+    </div>
+
+     <div class="carte">
+            <h3 class="carte__titre">Cours 4</h3>
+             <p class="carte__description"> Le cours 4 consiste à ...</p>
+    </div>
+</div>
+
+  <div class="sessionQuatre">Session 4
+     <div class="carte">
+            <h3 class="carte__titre">Cours 1</h3>
+             <p class="carte__description"> Le cours 1 consiste à ...</p>
+    </div>
+
+     <div class="carte">
+            <h3 class="carte__titre">Cours 2</h3>
+             <p class="carte__description"> Le cours 2 consiste à ...</p>
+    </div>
+
+     <div class="carte">
+            <h3 class="carte__titre">Cours 3</h3>
+             <p class="carte__description"> Le cours 3 consiste à ...</p>
+    </div>
+
+     <div class="carte">
+            <h3 class="carte__titre">Cours 4</h3>
+             <p class="carte__description"> Le cours 4 consiste à ...</p>
+    </div>
+</div>
+
+  <div class="sessionCinq">Session 5
+     <div class="carte">
+            <h3 class="carte__titre">Cours 1</h3>
+             <p class="carte__description"> Le cours 1 consiste à ...</p>
+    </div>
+
+     <div class="carte">
+            <h3 class="carte__titre">Cours 2</h3>
+             <p class="carte__description"> Le cours 2 consiste à ...</p>
+    </div>
+
+     <div class="carte">
+            <h3 class="carte__titre">Cours 3</h3>
+             <p class="carte__description"> Le cours 3 consiste à ...</p>
+    </div>
+
+     <div class="carte">
+            <h3 class="carte__titre">Cours 4</h3>
+             <p class="carte__description"> Le cours 4 consiste à ...</p>
+    </div>
+</div>
+
+  <div class="sessionSix">Session 6
+     <div class="carte">
+            <h3 class="carte__titre">Cours 1</h3>
+             <p class="carte__description"> Le cours 1 consiste à ...</p>
+    </div>
+
+     <div class="carte">
+            <h3 class="carte__titre">Cours 2</h3>
+             <p class="carte__description"> Le cours 2 consiste à ...</p>
+    </div>
+
+     <div class="carte">
+            <h3 class="carte__titre">Cours 3</h3>
+             <p class="carte__description"> Le cours 3 consiste à ...</p>
+    </div>
+
+     <div class="carte">
+            <h3 class="carte__titre">Cours 4</h3>
+             <p class="carte__description"> Le cours 4 consiste à ...</p>
+    </div>
+</div>
+
+
+<?php
+get_sidebar();
+get_footer();
